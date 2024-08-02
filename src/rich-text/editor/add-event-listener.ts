5fba757e3e1c46b4037030cc8f996269cee72de3
lizhi <>
2024-08-02 20:50:43 +0800

    fix: tsc检查问题

--- src/rich-text/editor/add-event-listener.ts
+++ src/rich-text/editor/add-event-listener.ts
@@ -2,7 +2,7 @@ import { EditorInterface } from "..";
 
 export const addEventListener: EditorInterface['addEventListener'] = (editor, type, fn) => {
     if (editor.__events[type]) {
-        editor.__events[type].add(fn)
+        editor.__events[type]!.add(fn)
     } else {
         editor.__events[type] = new Set([fn])
     }
@@ -10,7 +10,7 @@ export const addEventListener: EditorInterface['addEventListener'] = (editor, ty
 
 export const execEvent: EditorInterface['execEvent'] = (editor, type) => {
     if (editor.__events[type]) {
-        for (const fn of editor.__events[type]) {
+        for (const fn of editor.__events[type]!) {
             fn()
         }
     }
--- src/rich-text/editor/remove-event-listener.ts
+++ src/rich-text/editor/remove-event-listener.ts
@@ -2,6 +2,6 @@ import { EditorInterface } from ".."
 
 export const removeEventListener: EditorInterface['addEventListener'] = (editor, type, fn) => {
     if (editor.__events[type]?.has(fn)) {
-        editor.__events[type].delete(fn)
+        editor.__events[type]!.delete(fn)
     }
 }
